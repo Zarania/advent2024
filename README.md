d652ace24fd89b5a0209d0d4d6fcdaf319b08608
Zarania <scott.a.reinhardt@gmail.com>
2024-12-03 01:47:48 -0500

    Swapped to match over if else if

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | :---: | :---: | :---:  |
 | [Day 1](./src/bin/01.rs) | `39.4µs` | `36.2µs` |
 | [Day 2](./src/bin/02.rs) | `150.8µs` | `157.5µs` |
-| [Day 3](./src/bin/03.rs) | `34.6µs` | `48.7µs` |
+| [Day 3](./src/bin/03.rs) | `30.8µs` | `31.6µs` |
 
-**Total: 0.47ms**
+**Total: 0.45ms**
 <!--- benchmarking table --->
--- src/bin/03.rs
+++ src/bin/03.rs
@@ -5,14 +5,13 @@ pub fn part_one(input: &str) -> Option<u32> {
     let mut sum = 0u32;
     for mut i in 0..input.len() - 7 {
         if &input[i..i + 4] == "mul(" {
-            let j = (i + 4..input.len())
-                .find(|c| &input[*c..*c + 1] == ")")
-                .unwrap();
-            if let Some(index) = input[i + 4..j].find(',') {
-                let (a, b) = input[i + 4..j].split_at(index);
-                sum += a.parse::<u32>().unwrap_or(0) * b[1..].parse::<u32>().unwrap_or(0);
+            if let Some(j) = (i + 4..input.len().min(i + 12)).find(|c| &input[*c..*c + 1] == ")") {
+                if let Some(index) = input[i + 4..j].find(',') {
+                    let (a, b) = input[i + 4..j].split_at(index);
+                    sum += a.parse::<u32>().unwrap_or(0) * b[1..].parse::<u32>().unwrap_or(0);
+                }
+                i += j;
             }
-            i += j;
         }
     }
 
@@ -24,21 +23,25 @@ pub fn part_two(input: &str) -> Option<u32> {
     let mut sum = 0u32;
     let mut enabled = true;
     for mut i in 0..input.len() - 7 {
-        if enabled && &input[i..i + 4] == "mul(" {
-            let j = (i + 4..input.len())
-                .find(|c| &input[*c..*c + 1] == ")")
-                .unwrap();
-            if let Some(index) = input[i + 4..j].find(',') {
-                let (a, b) = input[i + 4..j].split_at(index);
-                sum += a.parse::<u32>().unwrap_or(0) * b[1..].parse::<u32>().unwrap_or(0);
+        match &input[i..i + 4] {
+            "mul(" if enabled => {
+                if let Some(j) = (i + 4..input.len()).find(|c| &input[*c..*c + 1] == ")") {
+                    if let Some(index) = input[i + 4..j].find(',') {
+                        let (a, b) = input[i + 4..j].split_at(index);
+                        sum += a.parse::<u32>().unwrap_or(0) * b[1..].parse::<u32>().unwrap_or(0);
+                    }
+                    i += j;
+                }
             }
-            i += j;
-        } else if &input[i..i + 4] == "do()" {
-            enabled = true;
-            i += 4;
-        } else if &input[i..i + 7] == "don't()" {
-            enabled = false;
-            i += 7;
+            "do()" => {
+                enabled = true;
+                i += 4;
+            }
+            "don'" => {
+                enabled = false;
+                i += 7;
+            }
+            _ => {}
         }
     }
 
