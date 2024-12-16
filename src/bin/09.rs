2b85f067e7f498ce3c48eac4e201cc851e86bba2
Zarania <scott.a.reinhardt@gmail.com>
2024-12-16 00:56:28 -0500

    Added day 10, lots of caching.  P1 can probably be cleaned up

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -3,78 +3,113 @@ use itertools::Itertools;
 advent_of_code::solution!(9);
 
 pub fn part_one(input: &str) -> Option<usize> {
-    let mut drive = input.chars().chunks(2).into_iter().enumerate().flat_map(|(i, mut chunk)| {
-        let file = chunk.next().unwrap() as u32 - '0' as u32;
-        let free = chunk.next().unwrap_or('0') as u32 - '0' as u32;
-        
-        (0..file).map(move |_| Some(i)).chain((0..free).map(|_| None))
-    }).collect::<Vec<_>>();
+    let mut drive = input
+        .chars()
+        .chunks(2)
+        .into_iter()
+        .enumerate()
+        .flat_map(|(i, mut chunk)| {
+            let file = chunk.next().unwrap() as u32 - '0' as u32;
+            let free = chunk.next().unwrap_or('0') as u32 - '0' as u32;
+
+            (0..file)
+                .map(move |_| Some(i))
+                .chain((0..free).map(|_| None))
+        })
+        .collect::<Vec<_>>();
 
     let mut empty = 0;
     let mut last = drive.len() - 1;
 
     loop {
         empty = drive[empty..].iter().position(|&x| x.is_none()).unwrap() + empty;
-        last = last.checked_sub(drive[..=last].iter().rev().position(|&x| x.is_some()).unwrap()).unwrap_or(1);
+        last = last
+            .checked_sub(
+                drive[..=last]
+                    .iter()
+                    .rev()
+                    .position(|&x| x.is_some())
+                    .unwrap(),
+            )
+            .unwrap_or(1);
 
         if empty >= last {
             break;
         }
-        
+
         drive[empty] = drive[last];
         drive[last] = None;
         last -= 1;
     }
 
-    Some(drive.iter().enumerate().map(|(i, x)| {
-        if x.is_some() {
-            x.unwrap() * i
-        } else {
-            0
-        }
-    }).sum())
+    Some(
+        drive
+            .iter()
+            .enumerate()
+            .map(|(i, x)| if x.is_some() { x.unwrap() * i } else { 0 })
+            .sum(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    let mut drive = input.chars().chunks(2).into_iter().enumerate().flat_map(|(i, mut chunk)| {
-        let file = chunk.next().unwrap() as u32 - '0' as u32;
-        let free = chunk.next().unwrap_or('0') as u32 - '0' as u32;
-        
-        (0..file).map(move |_| Some(i)).chain((0..free).map(|_| None))
-    }).collect::<Vec<_>>();
+    let mut drive = input
+        .chars()
+        .chunks(2)
+        .into_iter()
+        .enumerate()
+        .flat_map(|(i, mut chunk)| {
+            let file = chunk.next().unwrap() as u32 - '0' as u32;
+            let free = chunk.next().unwrap_or('0') as u32 - '0' as u32;
+
+            (0..file)
+                .map(move |_| Some(i))
+                .chain((0..free).map(|_| None))
+        })
+        .collect::<Vec<_>>();
 
     let mut last = drive.len() - 1;
 
     loop {
-        last = last.checked_sub(drive[..=last].iter().rev().position(|&x| x.is_some()).unwrap()).unwrap_or(0);
-        
+        last = last
+            .saturating_sub(
+                drive[..=last]
+                    .iter()
+                    .rev()
+                    .position(|&x| x.is_some())
+                    .unwrap(),
+            );
+
         if last == 0 {
             break;
         }
 
         let file = drive[last];
-        let file_size = drive[..last].iter().rev().take_while(|&&x| x.is_some() && x.unwrap() == file.unwrap()).count();
+        let file_size = drive[..last]
+            .iter()
+            .rev()
+            .take_while(|&&x| x.is_some() && x.unwrap() == file.unwrap())
+            .count();
 
-        
         for i in 0..(last - file_size) {
             if drive[i..=i + file_size].iter().all(|&x| x.is_none()) {
                 drive[i..=i + file_size].iter_mut().for_each(|x| *x = file);
-                drive[last - file_size..=last].iter_mut().for_each(|x| *x = None);
+                drive[last - file_size..=last]
+                    .iter_mut()
+                    .for_each(|x| *x = None);
                 break;
             }
         }
-        
-        last = last.checked_sub(file_size + 1).unwrap_or(0);
+
+        last = last.saturating_sub(file_size + 1);
     }
 
-
-    Some(drive.iter().enumerate().map(|(i, x)| {
-        if x.is_some() {
-            x.unwrap() * i
-        } else {
-            0
-        }
-    }).sum())
+    Some(
+        drive
+            .iter()
+            .enumerate()
+            .map(|(i, x)| if x.is_some() { x.unwrap() * i } else { 0 })
+            .sum(),
+    )
 }
 
 #[cfg(test)]
---
+++ src/bin/10.rs
@@ -0,0 +1,174 @@
+use rustc_hash::FxHashSet;
+
+advent_of_code::solution!(10);
+
+const DIRECTIONS: [(i32, i32); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
+
+fn path_length(
+    topological: &Vec<Vec<u32>>,
+    cache: &mut Vec<Vec<(bool, FxHashSet<(usize, usize)>)>>,
+    current: (usize, usize),
+) -> FxHashSet<(usize, usize)> {
+    if cache[current.1][current.0].0 {
+        return cache[current.1][current.0].1.clone();
+    }
+
+    if topological[current.1][current.0] == 9 {
+        cache[current.1][current.0] = (true, FxHashSet::default());
+        cache[current.1][current.0].1.insert((current.0, current.1));
+        return cache[current.1][current.0].1.clone();
+    }
+
+    let mut paths = FxHashSet::default();
+    for direction in DIRECTIONS.iter() {
+        let next = (
+            current.0 as i32 + direction.0,
+            current.1 as i32 + direction.1,
+        );
+        if next.0 >= 0
+            && next.0 < topological[0].len() as i32
+            && next.1 >= 0
+            && next.1 < topological.len() as i32
+            && topological[next.1 as usize][next.0 as usize]
+                == topological[current.1][current.0] + 1
+        {
+            path_length(topological, cache, (next.0 as usize, next.1 as usize))
+                .iter()
+                .for_each(|p| {
+                    paths.insert(*p);
+                });
+        }
+    }
+    paths
+}
+
+fn path_length2(
+    topological: &Vec<Vec<u32>>,
+    cache: &mut Vec<Vec<i32>>,
+    current: (usize, usize),
+) -> i32 {
+    if cache[current.1][current.0] > 0 {
+        return cache[current.1][current.0];
+    }
+
+    if topological[current.1][current.0] == 9 {
+        return 1;
+    }
+
+    let mut paths = 0;
+    for direction in DIRECTIONS.iter() {
+        let next = (
+            current.0 as i32 + direction.0,
+            current.1 as i32 + direction.1,
+        );
+        if next.0 >= 0
+            && next.0 < topological[0].len() as i32
+            && next.1 >= 0
+            && next.1 < topological.len() as i32
+            && topological[next.1 as usize][next.0 as usize]
+                == topological[current.1][current.0] + 1
+        {
+            paths += path_length2(topological, cache, (next.0 as usize, next.1 as usize));
+        }
+    }
+    cache[current.1][current.0] = paths;
+    paths
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let topological = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| c as u32 - '0' as u32)
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let mut cache =
+        vec![vec![(false, FxHashSet::default()); topological[0].len()]; topological.len()];
+
+    let total = topological
+        .iter()
+        .enumerate()
+        .map(|(y, row)| {
+            row.iter()
+                .enumerate()
+                .map(|(x, cell)| {
+                    if *cell == 0 {
+                        path_length(&topological, &mut cache, (x, y)).len() as u32
+                    } else {
+                        0
+                    }
+                })
+                .sum::<u32>()
+        })
+        .sum::<u32>()
+        .into();
+
+    total
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let topological = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| c as u32 - '0' as u32)
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let mut cache = vec![vec![0; topological[0].len()]; topological.len()];
+
+    let total = topological
+        .iter()
+        .enumerate()
+        .map(|(y, row)| {
+            row.iter()
+                .enumerate()
+                .map(|(x, cell)| {
+                    if *cell == 0 {
+                        path_length2(&topological, &mut cache, (x, y))
+                    } else {
+                        0
+                    }
+                })
+                .sum::<i32>() as u32
+        })
+        .sum::<u32>()
+        .into();
+
+    total
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(36));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(81));
+    }
+
+    #[test]
+    #[ignore]
+    fn solve_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(794));
+    }
+
+    #[test]
+    #[ignore]
+    fn solve_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(1706));
+    }
+}
